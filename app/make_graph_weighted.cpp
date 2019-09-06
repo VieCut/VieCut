755c1dc75da82efc8665f92b6beb2882a2034cbd
Alexander Noe <alexander.noe@univie.ac.at>
2019-09-06 16:10:52 +0200

    Finish make_graph_weighted script, fix uint32_t / uint64_t issues.

--- app/make_graph_weighted.cpp
+++ app/make_graph_weighted.cpp
@@ -20,12 +20,16 @@ int main(int argn, char** argv) {
 
     tlx::CmdlineParser cmdl;
 
+    random_functions::setSeed(time(NULL));
+
     auto cfg = configuration::getConfig();
 
     std::string output_path = "";
     size_t min = 0;
     size_t max = 0;
     size_t multi = 0;
+
+    cfg->seed = UNDEFINED_NODE;
 
     cmdl.add_param_string("graph", cfg->graph_filename, "path to graph file");
     cmdl.add_param_string("output_path", output_path, "output path");
@@ -34,25 +38,40 @@ int main(int argn, char** argv) {
     cmdl.add_param_size_t(
         "multi", multi,
         "multiply weight of one (outgoing) edge per vertex by this factor");
+    cmdl.add_size_t('s', "seed", cfg->seed,
+                    "random seed (using time otherwise)");
 
     if (!cmdl.process(argn, argv)) {
         LOG << "Error in command line processing!";
         exit(1);
     }
 
-    std::unordered_set<NodeID> heavy_edges;
-    std::unordered_map<NodeID, EdgeWeight> edge_weights;
+    if (cfg->seed != UNDEFINED_NODE) {
+        random_functions::setSeed(cfg->seed);
+    }
+
+    LOG1 << "Using seed " << random_functions::getSeed();
 
     auto G = graph_io::readGraphWeighted(cfg->graph_filename);
 
+    std::vector<bool> covered(G->number_of_nodes());
+    std::unordered_set<uint64_t> heavy_edges;
+    std::unordered_map<uint64_t, EdgeWeight> edge_weights;
+
     size_t edges = 0;
     for (NodeID n : G->nodes()) {
+        // if (!covered[n]) {
         EdgeID e_highweight = random_functions::nextInt(
             G->get_first_edge(n), G->get_first_invalid_edge(n) - 1);
+        NodeID t = G->getEdgeTarget(e_highweight);
 
         edges++;
-        NodeID t = G->getEdgeTarget(e_highweight);
-        heavy_edges.insert(contraction::get_uint64_from_pair(n, t));
+        uint64_t pair = contraction::get_uint64_from_pair(n, t);
+        heavy_edges.insert(pair);
+        covered[t] = true;
+        covered[n] = true;
+
+//        }
     }
 
     for (NodeID n : G->nodes()) {

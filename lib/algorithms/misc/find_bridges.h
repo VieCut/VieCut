7c2f0b784a01810f4db1e7b4adbafd3dc755043c
Alexander Noe <alexander.noe@univie.ac.at>
2019-11-29 15:55:26 +0100

    iterative bridge detection (to avoid huge recursion depth)

--- lib/algorithms/misc/find_bridges.h
+++ lib/algorithms/misc/find_bridges.h
@@ -9,6 +9,7 @@
  * Published under the MIT license in the LICENSE file.
  *****************************************************************************/
 
+#include <algorithms/multicut/multicut_problem.h>
 #include <data_structure/mutable_graph.h>
 
 class find_bridges {
@@ -19,35 +20,70 @@ class find_bridges {
           discovered(G->n(), UNDEFINED_NODE),
           parent(G->n(), UNDEFINED_NODE),
           lowest(G->n(), UNDEFINED_NODE),
-          step(0) { };
+          step(0) { }
 
-    void visit(NodeID n) {
-        // DFS, check whether alternative path exists when backtracking
-        visited[n] = true;
-        step++;
-        discovered[n] = step;
-        lowest[n] = step;
-        for (EdgeID e : G->edges_of(n)) {
-            NodeID t = G->getEdgeTarget(n, e);
-            if (!visited[t]) {
-                parent[t] = n;
-                visit(t);
-                lowest[n] = std::min(lowest[n], lowest[t]);
-                if (lowest[t] > discovered[n]) {
-                    LOG1 << "THERE IS A BRIDGE BETWEEN " << n << " and " << t
-                    << "edge id " << e;
+    void findAllBridgesInCC(NodeID vtx) {
+        std::vector<NodeID> path;
+        path.emplace_back(vtx);
+        parent[vtx] = vtx;
+        stack.push(vtx);
+        while (!stack.empty()) {
+            NodeID n = stack.top();
+            stack.pop();
+            if (visited[n])
+                continue;
+
+            while (path.size() > 1 && path.back() != parent[n]) {
+                // explicit backtracking to avoid recursion
+                NodeID b = path.back();
+                path.pop_back();
+                for (EdgeID e : G->edges_of(b)) {
+                    NodeID t = G->getEdgeTarget(b, e);
+                    if (parent[t] == b) {
+                        lowest[b] = std::min(lowest[b], lowest[t]);
+                        if (lowest[t] > discovered[b]) {
+                            bridges.emplace_back(b, e);
+                        }
+                    } else {
+                        if (t != parent[b]) {
+                            lowest[b] = std::min(lowest[b], discovered[t]);
+                        }
+                    }
                 }
-            } else if (t != parent[n]) {
-                lowest[n] = std::min(lowest[n], discovered[t]);
+            }
+
+            path.emplace_back(n);
+            // DFS, check whether alternative path exists when backtracking
+            visited[n] = true;
+            step++;
+            discovered[n] = step;
+            lowest[n] = step;
+            for (EdgeID e : G->edges_of(n)) {
+                NodeID t = G->getEdgeTarget(n, e);
+                if (!visited[t]) {
+                    parent[t] = n;
+                    stack.push(t);
+                }
             }
         }
     }
 
-    void find_all_bridges() {
+    void findAllBridges() {
         LOG1 << "trying to find bridges!";
-        for (NodeID n : G->nodes()) {
-            if (!visited[n]) {
-                visit(n);
+        for (NodeID vtx : G->nodes()) {
+            if (!visited[vtx]) {
+                findAllBridgesInCC(vtx);
+            }
+        }
+    }
+
+    void checkTerminalSides(std::vector<terminal> terminals) {
+        for (const auto& [n, e] : bridges) {
+            LOG1 << "bridge " << n << " - " << e;
+            for (const auto& t : terminals) {
+                NodeID tpos = t.position;
+                bool on_right = (lowest[tpos] <= discovered[n]);
+                LOG1 << "terminal " << tpos << " is " << on_right;
             }
         }
     }
@@ -58,5 +94,8 @@ class find_bridges {
     std::vector<NodeID> discovered;
     std::vector<NodeID> parent;
     std::vector<NodeID> lowest;
+
+    std::stack<NodeID> stack;
+    std::vector<std::pair<NodeID, EdgeID> > bridges;
     size_t step;
 };
